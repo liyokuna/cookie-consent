b0f2a1b1d0cd57fe4458bc94a8232cb1e438c879
liyokuna <liyokuna@yahoo.fr>
2019-05-28 00:41:47 +0200

    updating travis

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to cookie-consent!');
+    expect(page.getTitleText()).toEqual('Cookie Consent Banner');
   });
 
   afterEach(async () => {
