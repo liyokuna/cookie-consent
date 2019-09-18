de12e661e95e448212f24cd42c6ecbaaef183bb8
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-09-18 22:52:21 +0000

    Deploy cookie-consent.org to github.com/liyokuna/cookie-consent.git:gh-pages

--- projects/cookie-service/src/public-api.ts
+++
@@ -1,8 +0,0 @@
-/*
- * Public API Surface of cookie-service
- */
-
-export * from './lib/service/cookie-config.service';
-export * from './lib/interface/cookie-config.interface';
-export * from './lib/cookie-service.component';
-export * from './lib/cookie-service.module';
